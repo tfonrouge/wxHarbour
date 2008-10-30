8f0fd02fe2ceaeeca8034fd9c45471af57af004e
Teo Fonrouge <tfonrouge@gmail.com>
2008-10-30 04:12:24 +0000

    2008-10-29 21:21 UTC-0600 Teo Fonrouge (teo/at/windtelsoft/dot/com)   + src/wxbase/baseclasses/wx_event.cpp   + src/wxbase/baseclasses/wx_gridevent.cpp   + src/wxbase/baseclasses/wxhevent.prg   + src/wxbase/baseclasses/wxhgridevent.prg     * added missing sources on previous commit

---
+++ wxHarbour/src/wxbase/baseclasses/wx_gridevent.cpp
@@ -0,0 +1,62 @@
+/*
+  wxHarbour: a portable GUI for [x]Harbour Copyright (C) 2008 Teo Fonrouge
+
+  This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  (C) 2008 Teo Fonrouge <teo@windtelsoft.com>
+*/
+
+/*
+  wx_GridEvent: Implementation
+  Teo. Mexico 2008
+*/
+
+#include "wx/wx.h"
+
+#include "wxh.h"
+
+/*
+  GetCol
+  Teo. Mexico 2008
+*/
+HB_FUNC( WXGRIDEVENT_GETCOL )
+{
+  PHB_ITEM pSelf = hb_stackSelfItem();
+  wxGridEvent *gridEvent = (wxGridEvent *) wxh_ItemListGetWX( pSelf );
+  if( pSelf && gridEvent )
+    hb_retni( gridEvent->GetCol() );
+  else
+    hb_ret();
+}
+
+/*
+  GetRow
+  Teo. Mexico 2008
+*/
+HB_FUNC( WXGRIDEVENT_GETROW )
+{
+  PHB_ITEM pSelf = hb_stackSelfItem();
+  wxGridEvent *gridEvent = (wxGridEvent *) wxh_ItemListGetWX( pSelf );
+  if( pSelf && gridEvent )
+    hb_retni( gridEvent->GetRow() );
+  else
+    hb_ret();
+}
+
+/*
+  Selecting
+  Teo. Mexico 2008
+*/
+HB_FUNC( WXGRIDEVENT_SELECTING )
+{
+  PHB_ITEM pSelf = hb_stackSelfItem();
+  wxGridEvent *gridEvent = (wxGridEvent *) wxh_ItemListGetWX( pSelf );
+  if( pSelf && gridEvent )
+    hb_retl( gridEvent->Selecting() );
+  else
+    hb_ret();
+}
