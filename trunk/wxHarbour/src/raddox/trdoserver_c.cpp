ec4235dc85c4f3b6bb26cd32883e7f0191568634
tfonrouge <tfonrouge@ce071519-8a5c-4472-b514-262ff220dd64>
2009-02-04 19:16:57 +0000

    2009-02-04 13:16 UTC-0600 Teo Fonrouge (teo/at/windtelsoft/dot/com)   * GNUmakefile     * updated makefile   * src/raddox/trdoserver_c.cpp     * renamed hb_dbgObjSendMessage() to hb_dbg_objSendMessage()

--- trunk/wxHarbour/src/raddox/trdoserver_c.cpp
+++ trunk/wxHarbour/src/raddox/trdoserver_c.cpp
@@ -254,5 +254,5 @@ HB_FUNC( __RDOSENDMESSAGE )
   PHB_ITEM pObject = hb_param( 1, HB_IT_ANY );
   PHB_ITEM pMessage = hb_param( 2, HB_IT_SYMBOL | HB_IT_STRING );
 
-  hb_dbgObjSendMessage( 0, pObject, pMessage, 2 );
+  hb_dbg_objSendMessage( 0, pObject, pMessage, 2 );
 }
