4a9a489636460e96f84445d5008ceb55007f8c23
tfonrouge <tfonrouge@ce071519-8a5c-4472-b514-262ff220dd64>
2009-05-26 14:37:59 +0000

    2009-05-26 09:36 UTC-0500 Teo Fonrouge (teo/at/windtelsoft/dot/com)   * build_bkls.sh   * config/mkdirstempl.bkl   * demos/dbExplorer/GNUmakefile   * demos/helloworld/GNUmakefile   * demos/helloworld/helloworld.bkl   * samples/arrbrowser/GNUmakefile   * samples/arrbrowser/arrbrowser.bkl   * samples/auinotebook/GNUmakefile   * samples/button/GNUmakefile   * samples/checkbox/GNUmakefile   * samples/choice/GNUmakefile   * samples/combobox/GNUmakefile   * samples/dbbrowser/GNUmakefile   * samples/dbbrowser/dbbrowser.bkl   * samples/gauge/GNUmakefile   * samples/grid/GNUmakefile   * samples/listbook/GNUmakefile   * samples/menu/GNUmakefile   * samples/menu/menu.bkl   * samples/networking/GNUmakefile   * samples/notebook/GNUmakefile   * samples/panel/GNUmakefile   * samples/radiobox/GNUmakefile   * samples/sayget/GNUmakefile   * samples/sizers/GNUmakefile   * samples/sizers/sizers.bkl   * samples/spinctrl/GNUmakefile   * samples/taskBarIcon/GNUmakefile   * samples/text/GNUmakefile   * samples/text/text.bkl   * samples/timer/GNUmakefile   * samples/treectrl/GNUmakefile   * tests/evtdefcheck/GNUmakefile   * tests/socketCli/GNUmakefile   * tests/socketSrv/GNUmakefile   + bin   + bin/run_App.sh     * added capability to create a minimal MacOS bundle to run the apps

---
+++ trunk/wxHarbour/bin/run_App.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+App_MacPath="./${1}.app/Contents"
+
+if [ `uname` == "Darwin" ] ; then
+    mkdir -p ${App_MacPath}/MacOS
+    ln -f ${1} ${App_MacPath}/MacOS/${1}
+    if [ ! -e ${App_MacPath}/Info.plist ] ; then 
+	cp ../../config/Info.plist ${App_MacPath}
+	SEDCMD=s/__APPNAME__/${1}/
+	sed -E -i "" ${SEDCMD} ${App_MacPath}/Info.plist
+    fi
+    ${App_MacPath}/MacOS/${1}
+else
+    ./${1}
+fi
