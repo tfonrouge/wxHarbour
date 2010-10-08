278599b49fcd28902525b5725dbd6802a0097977
tfonrouge <tfonrouge@ce071519-8a5c-4472-b514-262ff220dd64>
2010-10-08 02:37:18 +0000

    2010-10-07 21:37 UTC-0500 Teo Fonrouge (teo/at/windtelsoft/dot/com)   *  include/wxHarbour/wxbase/wx_ScrolledWindow.h   *  src/wxbase/baseclasses/wx_ScrolledWindow.cpp   *  src/wxbase/baseclasses/wxhScrolledWindow.prg      + added method's:        + New, CalcScrolledPosition, CalcUnscrolledPosition, EnableScrolling,          GetScrollPixelsPerUnit, GetViewStart, GetVirtualSize, IsRetained,          Scroll, SetScrollBars, SetScrollRate, SetTargetWindow

--- trunk/wxHarbour/include/wxHarbour/wxbase/wx_ScrolledWindow.h
+++ trunk/wxHarbour/include/wxHarbour/wxbase/wx_ScrolledWindow.h
@@ -3,7 +3,7 @@
  */
 
 /*
- wxHarbour: a portable GUI for [x]Harbour Copyright (C) 2006 Teo Fonrouge
+ wxHarbour: a portable GUI for [x]Harbour Copyright (C) 2010 Teo Fonrouge
  
  This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
  
@@ -11,7 +11,7 @@
  
  You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  
- (C) 2006 Teo Fonrouge <teo@windtelsoft.com>
+ (C) 2010 Teo Fonrouge <teo@windtelsoft.com>
  */
 
 
@@ -19,11 +19,13 @@
  wx_ScrolledWindow: Interface
  Teo. Mexico 2010
  */
-
-class wx_ScrolledWindow : public hbWindow<wxWindow>
+class wx_ScrolledWindow : public wxScrolledWindow
 {
 private:
 protected:
 public:
-    wx_Window( wxWindow* parent, wxWindowID id = wxID_ANY, const wxPoint& pos = wxPoint( -1, -1 ), const wxSize& size = wxSize( -1, -1 ), long style = wxDEFAULT_FRAME_STYLE, const wxString& name = wxT("") );
+
+    wx_ScrolledWindow(wxWindow* parent, wxWindowID id = -1, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxHSCROLL | wxVSCROLL, const wxString& name = _T("scrolledWindow") ) : wxScrolledWindow( parent, id, pos, size, style, name ) {};
+
+    ~wx_ScrolledWindow();
 };
